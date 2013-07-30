115879707f4d11e78ba446b96a68e1c9dcb93373
Sabeer Zaman <sabeer.zaman@gmail.com>
2013-07-30 02:05:40 -0400

    Render blank HTML form when a new Post View is initialized and rendered.

--- specs/View-Blogpost.spec.js
+++ specs/View-Blogpost.spec.js
@@ -6,4 +6,22 @@ describe( '(Blog)Post View (for src/views/post.js)', function() {
 	it( 'should be a Backbone View', function() {
 		expect( Blog.Views.Post.constructor ).toEqual( Backbone.View.constructor );
 	});
+
+	it( 'should throw an error if initialized without a Post Model', function() {
+		expect(function() {
+			new Blog.Views.Post();
+		}).toThrow( 'No model defined' );
+	});
+
+	describe( 'When rendered with a new Post model', function() {
+		it( 'should create a form with HTML input element per editable field', function() {
+			this.view = new Blog.Views.Post({ model: new Backbone.Model() });
+			var $el = this.view.render().$el;
+			
+			expect( this.view.$el ).toBe( 'form.post' );
+			expect( this.view.$el ).toContain( 'input.title' );
+			expect( this.view.$el ).toContain( 'textarea.body' );
+			expect( this.view.$el ).toContain( 'input.author' );
+		});
+	});
 });
--- src/views/post.js
+++ src/views/post.js
@@ -1,3 +1,20 @@
 var Blog = Blog || {};
 
-Blog.Views.Post = Backbone.View.extend({});
+Blog.Views.Post = Backbone.View.extend({
+	tagName: 'form',
+	className: 'post',
+
+	initialize: function() {
+		if ( !this.model )
+			throw 'No model defined';
+	},
+
+	render: function() {
+		this.$el.html(
+			'<input type="text" class="title">' +
+			'<input type="text" class="author">' +
+			'<textarea class="body"></textarea>'
+			);
+		return this;
+	}
+});
